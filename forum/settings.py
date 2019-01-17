cb287c4f9bbd06fa0447c4a5282d16aeb360e414
hui <3293844719@qq.com>
2019-01-17 20:54:37 +0800

    Feat: Add UserModel

--- forum/settings.py
+++ forum/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 或者使用 mysql.connector.django
+        'NAME': 'forum',
+        'USER': 'root',
+        # 'PASSWORD': os.environ.get('MYSQLROOTPWD'),
+        'PASSWORD': 'dh666436',
+
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
