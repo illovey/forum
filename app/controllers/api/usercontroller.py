cc5b537bf84736dfaa7a3ecc1b082649d30bc41a
hui <3293844719@qq.com>
2019-01-19 21:56:02 +0800

    Feat: save the user info

--- app/controllers/api/usercontroller.py
+++ app/controllers/api/usercontroller.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from app.models import Users
+
 def register(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
+
+        user = Users(username=username, password=password)
+        user.save()
 
         if username == "" or password == "":
             return JsonResponse({"code": -1, "info": "请填写用户名或者密码"})
