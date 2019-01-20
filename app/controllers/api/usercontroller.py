2a4482c8cc351c8a252107e91762bac7e5974911
hui <3293844719@qq.com>
2019-01-20 12:18:52 +0800

    Feat: encrypt the pwd

--- app/controllers/api/usercontroller.py
+++ app/controllers/api/usercontroller.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from app.models import Users
+from hashlib import sha1
 
 def register(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
 
-        user = Users(username=username, password=password)
+        # encryption
+        sh1 = sha1()
+        sh1.update(password.encode('utf-8'))
+        pwdd = sh1.hexdigest()
+        print(len(pwdd))
+        user = Users(username=username, password=pwdd)
         user.save()
 
         if username == "" or password == "":
--- app/models.py
+++ app/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Users(models.Model):
     username = models.CharField(max_length=50)
-    password = models.CharField(max_length=32)
+    password = models.CharField(max_length=40)
