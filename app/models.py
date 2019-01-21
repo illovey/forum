01ff564a9c8b50783f46dac0680972a3aaa46ab8
codinghuang <2812240764@qq.com>
2019-01-21 20:42:23 +0800

    Fix: add unique for username

--- app/models.py
+++ app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=40)
